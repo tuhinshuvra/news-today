4dfdbb57895b6c1a3a190ea21dbda72172121039
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2022-09-03 11:31:13 +0600

    news details added

--- scripts/newsToday.js
+++ scripts/newsToday.js
@@ -9,7 +9,7 @@ const getCategories = () => {
 }
 
 const displayCategories = (categories) => {
-    console.log(categories)
+    // console.log(categories)
 
     const categoriesContainer = document.getElementById('categories-container');
     let category_id;
@@ -27,8 +27,8 @@ const displayCategories = (categories) => {
         li.appendChild(span);
         categoriesContainer.appendChild(li);
     }
-
 }
+
 let categoryId;
 const getCategoryId = (event) => {
     categoryId = event.children[0].innerText;
@@ -39,11 +39,66 @@ const getCategoryId = (event) => {
 const getCategoryNews = (category_id) => {
     fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
         .then(response => response.json())
-        .then(data => displayCategoryNews(data));
+        .then(news => displayCategoryNews(news.data));
 }
 
-const displayCategoryNews = (news) => {
-    console.log(news);
+const displayCategoryNews = (newsList) => {
+    // console.log(newsList);
+
+    const categoryNewsContainer = document.getElementById('category-news-container');
+    categoryNewsContainer.innerHTML = '';
+
+    for (let news of newsList) {
+        console.log(news);
+        const { image_url, details, title, author, total_view } = news;
+        const { img, name, published_date } = author;
+
+        const catNewsDiv = document.createElement('div');
+        catNewsDiv.classList.add('card');
+        catNewsDiv.classList.add('mb-3');
+        catNewsDiv.innerHTML = `
+        <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="${image_url}" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold ">${title}</h5>
+                                <p class="card-text">${details.length > 200 ? details.slice(0, 200) + '...' : details}</p>                                
+                            </div>
+
+                            <div class=" card-body d-flex justify-content-between align-items-center">
+                                <div class=" d-flex justify-content-between align-items-center">
+                                    <div>
+                                        <img style="width:50px ;" class=" rounded-circle " src="${img}" alt="">
+                                    </div>
+                                    <div class="">
+                                        <p>${name}</p>
+                                        <p>${published_date}</p>
+                                    </div>
+                                </div>
+
+                                <div class=" d-flex justify-content-between">
+                                    <div> <i class="fa-regular fa-eye"></i></div>
+                                    <p>${total_view}</p>
+                                </div>
+                                <div class=" d-flex">
+                                    <i class="fa-regular fa-star-half-stroke"></i>
+                                    <i class="fa-regular fa-star"></i>
+                                    <i class="fa-regular fa-star"></i>
+                                    <i class="fa-regular fa-star"></i>
+                                    <i class="fa-regular fa-star"></i>
+                                </div>
+                                <div>
+                                    <i class="fa-solid fa-arrow-right"></i>
+                                </div>
+
+                            </div>
+                        </div>
+        </div>
+    `;
+        categoryNewsContainer.appendChild(catNewsDiv);
+    }
 
 }
 
